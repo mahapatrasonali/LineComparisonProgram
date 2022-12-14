063f88dd59db8812d9fefb0b1327efb99d37e506
[mahapatrasonali] <[sonangr2000@gmail.com]>
2022-12-14 21:28:25 +0530

    Greeting message

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LineComparisonProgram
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Line Comparision Computation Program");
+        }
+    }
+}
