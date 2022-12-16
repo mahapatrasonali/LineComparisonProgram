fc21fe1985bb9d258fe22fd3312859e9e9fc943e
[mahapatrasonali] <[sonangr2000@gmail.com]>
2022-12-16 13:19:42 +0530

    Greeting Message

--- Program.cs
+++ Program.cs
@@ -5,6 +5,8 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Line Comparision Computation Program");
+            LineComp.FindLine();
+
         }
     }
 }
